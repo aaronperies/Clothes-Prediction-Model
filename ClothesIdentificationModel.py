0376d55df487a818e75ff7c9c3f678bc7d0b81cb
Aaron Peries <50733765+aaronperies@users.noreply.github.com>
2020-03-27 18:02:35 +0530

    v 2.0
    Incomplete, training completed but plotting graph for each testing set is not completed, functions have been pasted from TensorFlow website beginner section.

--- ClothesIdentificationModel.py
+++ ClothesIdentificationModel.py
@@ -2,8 +2,8 @@ from __future__ import absolute_import, division, print_function, unicode_litera
 import tensorflow as tf            #importing tensorflow
 from tensorflow import keras as ks #import keras neural network library
 
-#import numpy as np
-#import matplotlib.pyplot as plt
+import numpy as np
+import matplotlib.pyplot as plt
 
 fashion_Dataset = ks.datasets.fashion_mnist                                             #importing the fashion dataset from the keras datasets
 (train_images, train_labels), (test_images, test_labels) = fashion_Dataset.load_data()  #structure the datasets in the format that they are imported in
@@ -37,10 +37,48 @@ model.compile(optimizer='adam',
               loss=tf.keras.losses.SparseCategoricalCrossentropy(from_logits=True),
               metrics=['accuracy'])
 
-model.fit(train_images, train_labels, epochs=15)
+model.fit(train_images, train_labels, epochs=15) #epochs increased to increase accuracy for the model
 
-# =============================================================================
-# print(len(train_labels))
-# print(len(test_labels))
-# print(tf.__version__) #prints the tensorflow version installed on the machine
-# =============================================================================
+test_loss, test_acc = model.evaluate(test_images,  test_labels, verbose=2)
+
+print('\nTest accuracy:', test_acc)
+
+probability_model = tf.keras.Sequential([model, 
+                                         tf.keras.layers.Softmax()])
+
+predictions = probability_model.predict(test_images)
+
+predictions[0]
+np.argmax(predictions[0])
+
+def plot_image(i, predictions_array, true_label, img):
+  predictions_array, true_label, img = predictions_array, true_label[i], img[i]
+  plt.grid(False)
+  plt.xticks([])
+  plt.yticks([])
+
+  plt.imshow(img, cmap=plt.cm.binary)
+
+  predicted_label = np.argmax(predictions_array)
+  if predicted_label == true_label:
+    color = 'blue'
+  else:
+    color = 'red'
+
+  plt.xlabel("{} {:2.0f}% ({})".format(class_names[predicted_label],
+                                100*np.max(predictions_array),
+                                class_names[true_label]),
+                                color=color)
+
+def plot_value_array(i, predictions_array, true_label):
+  predictions_array, true_label = predictions_array, true_label[i]
+  plt.grid(False)
+  plt.xticks(range(10))
+  plt.yticks([])
+  thisplot = plt.bar(range(10), predictions_array, color="#777777")
+  plt.ylim([0, 1])
+  predicted_label = np.argmax(predictions_array)
+
+  thisplot[predicted_label].set_color('red')
+  thisplot[true_label].set_color('blue')
+  
